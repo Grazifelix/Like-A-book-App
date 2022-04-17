ca8a72dde884ea5bd249e16073035394f227605c
Grazi Felix <grazielafelix2001@gmail.com>
2022-04-17 02:30:13 -0300

    Changing form designs

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:likeabook_app/src/view/login_page_view.dart';
 
-
 void main() {
   runApp(const MyApp());
 }
@@ -13,6 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'LikeABook',
       theme: ThemeData(primarySwatch: Colors.deepPurple),
       initialRoute: '/',
--- lib/src/view/login_page_view.dart
+++ lib/src/view/login_page_view.dart
@@ -12,46 +12,93 @@ class LoginHomePage extends StatefulWidget {
 class _LoginHomePageState extends State<LoginHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return Scaffold(
+        body: Container(
+      padding: const EdgeInsets.all(54.0),
+      color: const Color.fromARGB(255, 127, 116, 206),
       child: Column(
-        children: [
+        children: <Widget>[
           Image.asset('imagens/logo_imagem.jpeg'),
           Form(
               child: Column(
-            children: [
-              const Text(
-                'Email',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 18,
-                ),
-              ),
-              Card(
-                  child: TextFormField(
+            children: <Widget>[
+              // const Text(
+              //   'Email',
+              //   style: TextStyle(
+              //     color: Colors.black,
+              //     fontSize: 18,
+              //   ),
+              // ),
+              TextFormField(
                 validator: (value) => validarEmail(value!),
-              )),
-              const Text(
-                'Senha',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 18,
-                ),
+                decoration: const InputDecoration(hintText: 'Email'),
               ),
-              Card(
-                  child: TextFormField(
+              TextFormField(
+                cursorColor: Colors.blue,
                 validator: (value) => validarSenha(value!),
-              )),
-              Row(
-                children: [
-                  TextButton(onPressed: () => {}, child: const Text('Login')),
-                  TextButton(
-                      onPressed: () => {}, child: const Text('Cadastrar'))
-                ],
-              )
+                decoration: const InputDecoration(hintText: 'Senha'),
+              ),
+
+              //TextButton(onPressed: () => {}, child: const Text('Login')),
+              //TextButton(onPressed: () => {}, child: const Text('Cadastrar'))
             ],
-          ))
+          )),
+          Material(
+              color: const Color.fromARGB(204, 255, 255, 255),
+              borderRadius: const BorderRadius.all(
+                Radius.circular(30),
+              ),
+              child: InkWell(
+                borderRadius: const BorderRadius.all(
+                  Radius.circular(30),
+                ),
+                onTap: () => print('Login'),
+                child: Ink(
+                  height: 54,
+                  width: double.maxFinite,
+                  decoration: const BoxDecoration(
+                      color: Colors.black12,
+                      borderRadius: BorderRadius.all(Radius.circular(30))),
+                  child: const Center(
+                    child: Text(
+                      'Login',
+                      style: TextStyle(
+                        color: Color.fromARGB(255, 127, 116, 206),
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
+                ),
+              )),
+          Material(
+              color: const Color.fromARGB(204, 255, 255, 255),
+              borderRadius: const BorderRadius.all(
+                Radius.circular(30),
+              ),
+              child: InkWell(
+                borderRadius: const BorderRadius.all(
+                  Radius.circular(30),
+                ),
+                onTap: () => print('Cadastrar'),
+                child: Ink(
+                  height: 54,
+                  width: double.maxFinite,
+                  decoration: const BoxDecoration(
+                      color: Colors.black12,
+                      borderRadius: BorderRadius.all(Radius.circular(30))),
+                  child: const Center(
+                    child: Text(
+                      'Cadastrar',
+                      style: TextStyle(
+                        color: Color.fromARGB(255, 127, 116, 206),
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
+                ),
+              ))
         ],
       ),
-    );
+    ));
   }
 }
