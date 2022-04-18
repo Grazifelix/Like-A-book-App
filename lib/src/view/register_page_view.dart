0222242dfb5456aabe65e332a91d3956dd99717f
Grazi Felix <grazielafelix2001@gmail.com>
2022-04-18 18:11:46 -0300

    feat: creating new named route page

---
+++ lib/src/view/register_page_view.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class RegisterPage extends StatefulWidget {
+  const RegisterPage({Key? key}) : super(key: key);
+  static const routeName = "/cadastro";
+
+  @override
+  State<RegisterPage> createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.maxFinite,
+        decoration: const BoxDecoration(
+            gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          stops: [0.0, 0.4, 0.6, 1.0],
+          colors: [
+            Color.fromARGB(255, 99, 85, 207),
+            Color.fromARGB(255, 117, 102, 212),
+            Color.fromARGB(255, 123, 103, 255),
+            Color.fromARGB(255, 99, 85, 207)
+          ],
+        )),
+        padding: const EdgeInsets.all(54.0),
+        child: ListView(),
+      ),
+    );
+  }
+}
