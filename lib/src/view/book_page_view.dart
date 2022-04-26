c370e9a927d5cef31bf91c2933563fea80bf828a
Grazi Felix <grazielafelix2001@gmail.com>
2022-04-26 16:09:18 -0300

    feat: new book page screen

---
+++ lib/src/view/book_page_view.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class BookPage extends StatefulWidget {
+  const BookPage({Key? key}) : super(key: key);
+  static const routeName = '/book';
+
+  @override
+  State<BookPage> createState() => _BookPage();
+}
+
+class _BookPage extends State<BookPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        flexibleSpace: Container(
+            decoration: const BoxDecoration(
+                gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          stops: [0.0, 0.4, 0.6, 1.0],
+          colors: [
+            Color.fromARGB(255, 99, 85, 207),
+            Color.fromARGB(255, 117, 102, 212),
+            Color.fromARGB(255, 123, 103, 255),
+            Color.fromARGB(255, 99, 85, 207)
+          ],
+        ))),
+        actions: [
+          Row(
+            children: <Widget>[
+              IconButton(
+                onPressed: () => {},
+                icon: const Icon(Icons.exit_to_app_outlined),
+                tooltip: "Logout",
+              )
+            ],
+          ),
+        ],
+      ),
+      body: Container(),
+    );
+  }
+}
