92449dd40feeed36997f3897ab3bdafbab578d78
carlosgabriel311 <75148299+carlosgabriel311@users.noreply.github.com>
2022-05-05 10:13:42 -0300

    update: profile settings page

--- lib/src/view/profile_settings_view.dart
+++ lib/src/view/profile_settings_view.dart
@@ -9,6 +9,9 @@ class ProfileSettingsPage extends StatefulWidget {
 }
 
 class _ProfileSettingsPageState extends State<ProfileSettingsPage> {
+  bool check1 = false;
+  bool check2 = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,7 +33,8 @@ class _ProfileSettingsPageState extends State<ProfileSettingsPage> {
           Row(
             children: <Widget>[
               IconButton(
-                onPressed: () => Navigator.pushNamedAndRemoveUntil(context, '/home', (route) => false),
+                onPressed: () => Navigator.pushNamedAndRemoveUntil(
+                    context, '/home', (route) => false),
                 icon: const Icon(Icons.home_outlined),
                 tooltip: "home",
               )
@@ -38,7 +42,68 @@ class _ProfileSettingsPageState extends State<ProfileSettingsPage> {
           ),
         ],
       ),
-      body: Container(),
+      body: Column(
+        children: [
+          const Text('Modo da recomendação'),
+          const Divider(),
+          Row(
+            children: [
+              Checkbox(
+                shape: const CircleBorder(),
+                value: check1,
+                onChanged: (value) => setState(
+                  () {
+                    check1 = value!;
+                    check2 = !value;
+                  },
+                ),
+              ),
+              const Text('Modo Aleatorio'),
+            ],
+          ),
+          Row(
+            children: [
+              Checkbox(
+                shape: const CircleBorder(),
+                value: check2,
+                onChanged: (value) => setState(
+                  () {
+                    check2 = value!;
+                    check1 = !value;
+                  },
+                ),
+              ),
+              const Text('Modo Aleatorio'),
+            ],
+          ),
+          const Text('Alterar Meus Dados'),
+          const Divider(),
+          TextButton(
+            onPressed: () => {},
+            child: const Text('Alterar Nome'),
+          ),
+          TextButton(
+            onPressed: () => {},
+            child: const Text('Alterar Email'),
+          ),
+          TextButton(
+            onPressed: () => {},
+            child: const Text('Alterar Senha'),
+          ),
+          TextButton(
+            onPressed: () => {
+              Navigator.pushNamedAndRemoveUntil(
+                    context, '/login', (route) => false),
+            },
+            child: const Text(
+              'Sair',
+              style: TextStyle(
+                color: Colors.red,
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
