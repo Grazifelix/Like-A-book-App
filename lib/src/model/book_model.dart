6517c0cb41473b63247781f72ae82349cbe3a28b
Arthur Cabral <63428806+CabralArthur@users.noreply.github.com>
2022-05-16 00:20:53 -0300

    feat(book-model): inserting book model

--- lib/src/model/book_model.dart
+++ lib/src/model/book_model.dart
@@ -1,6 +1,22 @@
 class Book {
-  String bookId = '';
-  String name = '';
-  String description = '';
-  String author = '';
+  String _urlImage = '';
+  String _bookId;
+  String _title = '';
+  String _genre = '';
+  int _rating = 0;
+  String _description = '';
+  String _author = '';
+
+  Book(this._urlImage, this._bookId, this._title, this._author, this._genre,
+      this._description, this._rating);
+
+  String get getUrlImage => _urlImage;
+  String get getBookId => _bookId;
+  String get getTitle => _title;
+  String get getAuthor => _author;
+  int get getRating => _rating;
+  String get getDescription => _description;
+  String get getGenre => _genre;
 }
+
+List<Book> books = [];
